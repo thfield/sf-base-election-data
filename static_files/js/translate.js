62aa734c45f92deb4dedfc710b942d671da88cb8
thfield <thfield@gmail.com>
2015-05-07 00:08:30 -0700

    Working translation script

--- static_files/js/translate.js
+++ static_files/js/translate.js
@@ -4,14 +4,12 @@ function getData(callback){
 	$.getJSON(dataUrl, callback);
 }
 
-function getLang(obj,lang){
-	if (obj.name_i18n){
-		if (sfBED.phrases[obj.name_i18n][lang]){
-			return sfBED.phrases[obj.name_i18n][lang]
-		}else {
-			return sfBED.phrases[obj.name_i18n].en
-		}
-	}else return obj.name
+function lookupTranslation(phrase,lang){
+	if (sfBED.phrases[phrase][lang]){
+		return sfBED.phrases[phrase][lang]
+	}else {
+		return sfBED.phrases[phrase].en
+	}
 }
 
 var sfBED = {};
@@ -26,27 +24,9 @@ var langValue ="en";
 
 $( "#i18n-selector" ).change( function(){
 	langValue = $( "#i18n-selector" ).val();
+
   translatePhrases.each(function(i,value){
-		var objKey = getKey(value.dataset.i18n);
-		var translation = getLang(sfBED[objKey][value.dataset.i18n],langValue);
-		$(this).text(translation);
+		 var translation = lookupTranslation(value.dataset.i18n,langValue);
+		 $(this).text( '( '+translation+' )' );
 	})
 });
-
-function getKey(phrase){
-	var re = /(\w+?)_/;
-	switch (re.exec(phrase)[0]){
-		case "body_":
-			return "bodies";
-			break;
-		case "category_":
-			return "categories";
-			break;
-		case "office_":
-			return "offices";
-			break;
-		case "district_":
-			return "district";
-			break;
-		};
-}
